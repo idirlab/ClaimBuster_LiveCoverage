34d049e1d9920cf8f63b889dea18f4e0c9933e28
Aviral Saxena <64943664+AviralSaxena@users.noreply.github.com>
2024-01-07 05:22:33 -0600

    Add initial implementation of threshold slider

--- script.js
+++ script.js
@@ -15,3 +15,16 @@ function offClick() {
     onButton.style.color='black';
     offButton.style.color = 'white';
 }
+
+const slideValue = document.querySelector("span");
+const inputSlider = document.querySelector("input");
+inputSlider.oninput = (()=>{
+  let value = inputSlider.value;
+  slideValue.textContent = value;
+  slideValue.style.left = (value/2) + "%";
+  slideValue.classList.add("show");
+});
+inputSlider.onblur = (()=>{
+  slideValue.classList.remove("show");
+});
+
